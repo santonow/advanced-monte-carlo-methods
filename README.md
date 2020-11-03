558bb9a262c97da9f44463dcfb94485c2ff0244d
santonow <stas.antonowicz@gmail.com>
2020-11-03 13:52:15 +0100

    Added comparison plot generation, expanded README.md.

--- README.md
+++ README.md
@@ -4,6 +4,69 @@ This is a repository containing my assignments for Advanced Monte Carlo Methods 
 ## Exercise 1
 
 Write a sampler that samples from a discrete probability distribution based on:
-- cumulative distribution function *linear time done*
-- alias method. *done*
+- cumulative distribution function
+- alias method
+
+### Usage:
+
+To create `comparison.png` plot:
+```bash
+python exercise1.py compare <min_classes> <max_classes>
+```
+
+For example `python exercise1.py compare 1 10000` will create a following plot:
+
+![comparison.png](comparison.png)
+
+It will take roughly 18 seconds to create this plot.
+
+Looking at this plot it is clear that cdf method takes linear time relative to the
+number of classes, whereas Alias method takes constant (O(1)) time relative to the number of classes
+(the length of probability vector).
+
+To create a comparison between each method in terms of their validity
+(i.e. if the method's results are roughly the same and consistent with the
+probability vector specified) use the following script call:
+```bash
+python exercise1.py sample <n_classes> <n_samples>
+```
+
+For example following call: `python exercise1.py sample 4 10000` will output this:
+```python
+Class 1
+        Probability: 0.400000
+        Expected count: 4000
+        Alias method count: 4045
+        cdf method count: 4040
+Class 2
+        Probability: 0.300000
+        Expected count: 3000
+        Alias method count: 2947
+        cdf method count: 2979
+Class 3
+        Probability: 0.200000
+        Expected count: 2000
+        Alias method count: 1989
+        cdf method count: 2015
+Class 4
+        Probability: 0.100000
+        Expected count: 1000
+        Alias method count: 1019
+        cdf method count: 966
+```
+
+Usage as a package:
+```python
+from exercise1 import Sampler, sample
+
+# using a class
+sampler = Sampler(probabilities=[0.2, 0.2, 0.3, 0.1])
+print(sampler.sample(n_samples=10, method="alias"))
+print(sampler.sample(n_samples=10, method="cdf"))
+
+# using a function
+
+print(sample(n_samples=10, probabilities=[0.2, 0.2, 0.3, 0.1], method="alias"))
+print(sample(n_samples=10, probabilities=[0.2, 0.2, 0.3, 0.1], method="cdf"))
+```
 
--- exercise1.py
+++ exercise1.py
@@ -1,8 +1,12 @@
-import numpy as np
 from typing import Iterable
 import random
 import math
 from dataclasses import dataclass
+import sys
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 @dataclass
@@ -12,8 +16,32 @@ class Interval:
     label: int
 
 
+def sample(n_samples: int, probabilities: Iterable[float], method: str) -> Iterable[int]:
+    """Sample from discrete pd.
+
+    Parameters
+    ----------
+    n_samples: number of samples to sample
+    probabilities: an iterable (e.g. list) of probabilities of individual classes
+    method: either `alias` or `cdf`
+
+    Returns
+    -------
+    samples: samples drawn from distribution specified by probabilities argument
+    """
+    sampler = Sampler(probabilities, initialize=True)
+    return sampler.sample(n_samples, method)
+
+
 class Sampler:
-    def __init__(self, probabilities: Iterable[float]):
+    def __init__(self, probabilities: Iterable[float], initialize=False):
+        """
+
+        Parameters
+        ----------
+        probabilities: an iterable (e.g. list) of probabilities of individual classes
+        initialize: whether to preprocess probabilities for each of the methods
+        """
         if not math.isclose(np.sum(probabilities), 1):
             raise ValueError("Probabilities should sum to one!")
         self.probabilities = probabilities
@@ -24,11 +52,30 @@ class Sampler:
         self.n = len(self.prob_to_class)
 
         self.intervals = None
+        if initialize:
+            self.compute_alias_tables()
+            self.compute_intervals()
 
-    def sample(self, n_samples, method="alias"):
-        pass
+    def sample(self, n_samples: int, method: str = "alias") -> Iterable[int]:
+        """Sample from pd.
 
-    def compute_intervals(self):
+        Parameters
+        ----------
+        n_samples: number of samples to sample
+        method: either `alias` or `cdf`
+
+        Returns
+        -------
+        samples: samples drawn from distribution specified by probabilities argument
+        """
+        if method == "alias":
+            return self.alias_method(n_samples)
+        elif method == "cdf":
+            return self.bruteforce_cdf_method(n_samples)
+        else:
+            raise ValueError(f"Method {method} not recognized!")
+
+    def compute_intervals(self) -> None:
         self.intervals = []
         for i, prob in sorted(
             self.prob_to_class.items(), reverse=True, key=lambda x: x[1]
@@ -44,7 +91,7 @@ class Sampler:
                     )
                 )
 
-    def compute_alias_tables(self):
+    def compute_alias_tables(self) -> None:
         self.U = [0] * (self.n + 1)
         self.K = [None] * (self.n + 1)
         overfull = set()
@@ -76,7 +123,7 @@ class Sampler:
             else:
                 raise ValueError("Not posible")
 
-    def alias_method(self, n_samples):
+    def alias_method(self, n_samples: int) -> Iterable[int]:
         if not self.U:
             self.compute_alias_tables()
         samples = []
@@ -91,7 +138,7 @@ class Sampler:
                 samples.append(self.K[i])
         return samples
 
-    def bruteforce_cdf_method(self, n_samples):
+    def bruteforce_cdf_method(self, n_samples: int) -> Iterable[int]:
         if not self.intervals:
             self.compute_intervals()
         samples = []
@@ -104,18 +151,53 @@ class Sampler:
 
 
 if __name__ == "__main__":
-    from collections import Counter
-    vector_length = 50
-    n_samples = 100000
-    probs = np.array([random.randint(1, 100) for _ in range(vector_length)])
-    probs = probs / np.sum(probs)
-    sampler = Sampler(probs)
-    print(sampler.prob_to_class)
-    alias_results = Counter(sampler.alias_method(n_samples))
-    cdf_results = Counter(sampler.bruteforce_cdf_method(n_samples))
-    for i, probs in sampler.prob_to_class.items():
-        print(f"Class {i}")
-        print(f"\tProbability: {probs:.6f}")
-        print(f"\tExpected count: {math.floor(n_samples * probs)}")
-        print(f"\tAlias method count: {alias_results[i]}")
-        print(f"\tcdf method count: {cdf_results[i]}")
+    if len(sys.argv) == 1 or sys.argv[1] == "--help" or sys.argv[1] == "-h":
+        print("Usage:")
+        print("To createa comparison plot (saved to comparison.png):")
+        print(f"\tpython {sys.argv[0]} compare <min_classes> <max_classes>")
+        print("To sample with both methods and compare results using a random pd:")
+        print(f"\tpython {sys.argv[0]} sample <n_classes> <n_samples>")
+    elif sys.argv[1] == "compare":
+        n_samples = 100
+        min_classes = int(sys.argv[2])
+        max_classes = int(sys.argv[3])
+        alias_times = []
+        cdf_times = []
+        prob_vector_sampler = Sampler([1/max_classes] * max_classes, initialize=True)
+        unnormalized_probs = prob_vector_sampler.sample(max_classes, method="alias")
+        for n_classes in range(min_classes, max_classes + 1):
+            probs = np.array(unnormalized_probs[:n_classes])
+            probs = probs / np.sum(probs)
+            sampler = Sampler(probs, initialize=True)
+            t0 = time.time()
+            sampler.sample(n_samples, "alias")
+            alias_times.append((time.time() - t0) / n_samples)
+            t0 = time.time()
+            sampler.sample(n_samples, "cdf")
+            cdf_times.append((time.time() - t0) / n_samples)
+        fig, ax = plt.subplots(nrows=1, ncols=1)
+        ax.plot(list(range(min_classes, max_classes + 1)), alias_times, label="alias method")
+        ax.plot(list(range(min_classes, max_classes + 1)), cdf_times, label="cdf method")
+        ax.legend()
+        fig.savefig("comparison.png")
+        plt.close(fig)
+
+    elif sys.argv[1] == "sample":
+        from collections import Counter
+        vector_length = int(sys.argv[2])
+        n_samples = int(sys.argv[3])
+        prob_vector_sampler = Sampler([1/vector_length] * vector_length, initialize=True)
+        probs = np.array(prob_vector_sampler.sample(vector_length, method="alias"))
+        probs = probs / np.sum(probs)
+        sampler = Sampler(probs)
+        print(sampler.prob_to_class)
+        alias_results = Counter(sampler.alias_method(n_samples))
+        cdf_results = Counter(sampler.bruteforce_cdf_method(n_samples))
+        for i, probs in sampler.prob_to_class.items():
+            print(f"Class {i}")
+            print(f"\tProbability: {probs:.6f}")
+            print(f"\tExpected count: {math.floor(n_samples * probs)}")
+            print(f"\tAlias method count: {alias_results[i]}")
+            print(f"\tcdf method count: {cdf_results[i]}")
+    else:
+        print("Option not recognized, exiting.")
